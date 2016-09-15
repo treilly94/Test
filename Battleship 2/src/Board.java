361b273e3025a5fd889a0bb2b7711a4b9e815bc7
Tom <treilly94@live.com>
2016-09-15 14:45:28 +0000

    test push

--- Battleship 2/src/Board.java
+++ Battleship 2/src/Board.java
@@ -1,27 +1,27 @@
 
 public class Board {
-	
-	static int boardSize = 12; //size of game 
-	static String[][] playerBoardStatus = new String[boardSize][boardSize]; //locations within board 
-	static String[][] computerBoardStatus = new String[boardSize][boardSize]; //locations within board 
-	
-	public static void setBoards(){ //set the board to all empty at the start of the game 
+
+	static int boardSize = 12; //size of game
+	static String[][] playerBoardStatus = new String[boardSize][boardSize]; //locations within board
+	static String[][] computerBoardStatus = new String[boardSize][boardSize]; //locations within board
+
+	public static void setBoards(){ //set the board to all empty at the start of the game
 		for(int i = 0; i < boardSize; i++){
 			for(int j = 0; j < boardSize; j++){
-				playerBoardStatus[j][i] = Console.empty; 
-				computerBoardStatus[j][i] = Console.empty; 
+				playerBoardStatus[j][i] = Console.empty;
+				computerBoardStatus[j][i] = Console.empty;
 			}
 		}
 	}
-	
+
 	public static void drawPlayer1Board(){ //draw the Player board
 		System.out.println("Player 1");
-		for(int k = 0; k < boardSize; k++){ 
+		for(int k = 0; k < boardSize; k++){
 			System.out.print("\t");
 			System.out.print(k + 1); //guide //guide
 		}
 		System.out.println();
-		for(int i = 0; i < boardSize; i++){ 
+		for(int i = 0; i < boardSize; i++){
 			System.out.print(i + 1); //guide
 			for(int j = 0; j < boardSize; j++){ //x axis
 				System.out.print("\t");
@@ -32,12 +32,12 @@ public class Board {
 	}
 	public static void drawPlayer2Board(){ //draw player 2s  board
 		System.out.println("Player 2");
-		for(int k = 0; k < boardSize; k++){ 
+		for(int k = 0; k < boardSize; k++){
 			System.out.print("\t");
 			System.out.print(k + 1); //guide
 		}
 		System.out.println();
-		for(int i = 0; i < boardSize; i++){ 
+		for(int i = 0; i < boardSize; i++){
 			System.out.print(i + 1); //guide
 			for(int j = 0; j < boardSize; j++){ //x axis
 				System.out.print("\t");
@@ -46,15 +46,15 @@ public class Board {
 		System.out.println();
 		}
 	}
-	
+
 	public static void drawAIBoard(){ //draw the computer board
 		System.out.println("Computer");
-		for(int k = 0; k < boardSize; k++){ 
+		for(int k = 0; k < boardSize; k++){
 			System.out.print("\t");
 			System.out.print(k + 1); //guide
 		}
 		System.out.println();
-		for(int i = 0; i < boardSize; i++){ 
+		for(int i = 0; i < boardSize; i++){
 			System.out.print(i + 1); //guide
 			for(int j = 0; j < boardSize; j++){ //x axis
 				System.out.print("\t");
@@ -69,3 +69,5 @@ public class Board {
 		}
 	}
 }
+
+#test
